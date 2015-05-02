1a4a7731401de5065cb2afd7835a97af727db88f
adler-ua <adler.ua@gmail.com>
2015-05-02 11:44:56 +0300

    First half working version.

---
+++ AngularJS_Test/AngularJS_Test/Scripts/app/cardsController.js
@@ -0,0 +1,12 @@
+﻿var cardsControllers = angular.module('cardsControllers', []);
+
+cardsControllers.controller('cardsListController', ['$scope', '$http', function ($scope, $http) {
+        //$scope.cards = [{ id: 1 }, { id: 2 }];
+        $http.get("all.json").success(function (data) {
+            var cards = [];
+            angular.forEach(data, function(card) {
+                cards.push(card);
+            });
+            $scope.cards = cards;
+        });
+    }]);
